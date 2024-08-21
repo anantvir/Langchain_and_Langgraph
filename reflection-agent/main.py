ef5e649784c211085427b59d978c9de8871fd608
Anantvir Singh <anantvirsingh@Anantvirs-MacBook-Pro-2.local>
2024-08-21 17:26:35 -0400

    change directory structure

--- reflection-agent/main.py
+++ reflection-agent/main.py
@@ -50,9 +50,7 @@ if __name__ == "__main__":
 
             After a long wait, it's  here- making the implementation of agents across different models with function calling - super easy.
 
-            Made a video covering their newest blog post
-
-                                  """)
+            Made a video covering their newest blog post""")
     
     graph.invoke(inputs)
 
